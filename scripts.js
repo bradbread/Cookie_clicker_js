4cf5b6c4fc4b6683cb36576efcbfebc779c30e06
bradbread <48189304+bradbread@users.noreply.github.com>
2019-06-03 11:33:07 +1000

    added scaling upgrade costs and started on a hp bar

--- scripts.js
+++ scripts.js
@@ -7,13 +7,27 @@ var critValueUpgrade = document.getElementById("critValueUpgrade");
 var autoClickUpgrade = document.getElementById("autoClickUpgrade");
 var gameLog = document.getElementById("gameLog");
 var playerStats = document.getElementById("playerStats");
+var enemyHealth = document.getElementById("health");
 
-//variables
+//player stats
 var score = 0;
 var clickValue = 1;
 var critChance = 0;
-var critChanceUpgradeNo = 0;
 var critValue = 2;
+var autoClickNum = 0;
+var hp = 10;
+//enemies
+var evilCookie = {
+    hp:10,
+    attack: 1,
+    name:"evil cookie"
+}
+
+//upgrade costs
+var clickPrice = 15;
+var autoClickPrice = 1000;
+var critChancePrice = 100;
+var critMultiplierPrice = 1000;
 
 //actions
 cookie.onclick = cookieClicked;
@@ -30,16 +44,18 @@ function cookieClicked() {
 }
 
 function addScore(addedScore = 1) {
-   if ( Math.floor((Math.random() * 100) + 1) < critChance)
+   if ( Math.floor((Math.random() * 100) + 1) <= critChance)
    {
     log("Critical cookie score increased by: " + addedScore * critValue);
     score += addedScore * critValue;
+    enemyHealth.value -= addedScore * critValue;
     displayScore();
    } 
    else 
    {
     log("score increased by: " + addedScore);
     score += addedScore;
+    enemyHealth.value -= addedScore;
     displayScore();
    }
 }
@@ -49,13 +65,19 @@ function displayScore() {
 }
 
 function upgradeClick() {
-    if (score >= 15)
+    if (score >= clickPrice)
     {
         log("clickUpgraded");
-        score -= 15;
+        score -= clickPrice;
         clickValue += 1;
         displayStats();
         displayScore();
+        clickPrice = Math.round(clickPrice * 1.1);
+        clickUpgrade.innerHTML = "upgrade click: " + clickPrice;
+    }
+    else
+    {
+        log("not enough cookies to purchase.")
     }
     
 }
@@ -65,41 +87,68 @@ function updateScroll () {
 }
 
 function log(message) {
-    gameLog.innerHTML += "\n" + message;
+    gameLog.innerHTML += "<br>" + message;
     updateScroll();
 }
 
 function upgradeCritChance() {
-    if (score >= 100)
+    if (score >= critChancePrice)
     {
         log("critChanceUpgraded");
-        score -= 100;
+        score -= critChancePrice;
         critChance += 5;
         displayStats();
         displayScore();
+        critChancePrice = Math.round(critChancePrice * 2);
+        critChanceUpgrade.innerHTML = "upgrade critical chance: " + critChancePrice;
+    }
+    else
+    {
+        log("not enough cookies to purchase.")
     }
 }
 
 function upgradeCritValue () {
-    if (score >= 1000)
+    if (score >= critMultiplierPrice)
 	{
 		log("critMultiplierUpgraded");
-		score -= 1000;
+		score -= critMultiplierPrice;
         critValue += 1;
         displayStats();
         displayScore();
-	}
+        critMultiplierPrice = Math.round(critMultiplierPrice * 2.5);
+        critValueUpgrade.innerHTML = "upgrade critical multiplier: " + critMultiplierPrice;
+    }
+    else
+    {
+        log("not enough cookies to purchas.")
+    }
 }
 
 function displayStats() {
-	playerStats.innerHTML = "click power: " + clickValue + ", crit chance: " + critChance + "%, crit multiplier: " + critValue;
+	playerStats.innerHTML = "click power: " + clickValue + ", crit chance: " + critChance + "%, crit multiplier: " + critValue + ", autoclicker number: " + autoClickNum;
 }
 
 function upgradeAutoClick() {
-	window.setInterval(autoClick, 1000)
+    if(score >= autoClickPrice) 
+    {
+        log("autoClickUpgraded");
+        autoClickNum += 1;
+        score -= autoClickPrice;
+        window.setInterval(autoClick, 1000);
+        displayScore();
+        autoClickPrice *= 10;
+        autoClickUpgrade.innerHTML = "upgrade autoclick: " + autoClickPrice;
+
+    }
+    else
+    {
+        log("not enough cookies to purchase.")
+    }
+
 }
 
 function autoClick() {
-	log("auto");
-	cookieClicked();
+	log("autoClick");
+	addScore(clickValue);
 }
